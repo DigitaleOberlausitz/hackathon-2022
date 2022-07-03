41aeca8361c035ccf443ca5f5525e8b56256bc03
Deswerve Pushdere <s4+deswerve@digitale-oberlausitz.eu>
2022-07-03 01:51:57 +0000

    Switch to Onsen Page and restor own background

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import config from "./chatbot/config";
 import MessageParser from "./chatbot/MessageParser";
 import ActionProvider from "./chatbot/ActionProvider";
 import {useMarkdown} from "./useMarkdown";
+import {Page} from 'react-onsenui';
 
 // @ts-ignore
 import dynamicFileName from './main.md';
@@ -13,10 +14,10 @@ import dynamicFileName from './main.md';
 function App() {
     const mdComponent = useMarkdown(dynamicFileName);
     return (
-        <div className="App">
+        <Page>
             {mdComponent}
             <Chatbot config={config} messageParser={MessageParser} actionProvider={ActionProvider}/>
-        </div>
+        </Page>
     );
 }
 
