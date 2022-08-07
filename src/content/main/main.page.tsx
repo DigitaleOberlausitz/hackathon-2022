aee6dbdc7c01977ad707a8cc20d2a2027667eadc
Manuel Mauky <manuel.mauky@zeiss.com>
2022-08-07 12:12:47 +0200

    add poster to mainpage

--- src/content/main/main.page.tsx
+++ src/content/main/main.page.tsx
@@ -4,4 +4,23 @@ import Markdown from "../../Markdown"
 // @ts-ignore
 import mainMd from "./main.md"
 
-export const MainPage: FC = () => <Markdown dynamicFileName={mainMd} modifier="main" />
+import "./mainpage.css"
+
+export const MainPage: FC = () => (
+    <div id="mainpage">
+        <div id="mainpage_content">
+            <Markdown dynamicFileName={mainMd} modifier="main" />
+        </div>
+        <div id="mainpage_poster">
+            <a
+                target="_blank"
+                href="https://raw.githubusercontent.com/DigitaleOberlausitz/grafiken/master/Plakate/hackathon/2022/poster-hackathon-A3-web.png"
+            >
+                <img
+                    alt="Poster zum Hackathon mit den wesentlichen Informationen nochmal grafisch aufbereitet"
+                    src="https://raw.githubusercontent.com/DigitaleOberlausitz/grafiken/master/Plakate/hackathon/2022/poster-hackathon-A3-web.png"
+                />
+            </a>
+        </div>
+    </div>
+)
